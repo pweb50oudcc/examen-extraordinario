5275e1319bde75074aea2c14732dbef9da5e6b52
pweb50oudcc <85596321+pweb50oudcc@users.noreply.github.com>
2021-06-20 11:59:12 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,13 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyA6y5DufaKY0S4m2KEHgRpha54PmFsuhsA",
+      authDomain: "fbwebauth-66caf.firebaseapp.com",
+      projectId: "fbwebauth-66caf",
+      storageBucket: "fbwebauth-66caf.appspot.com",
+      messagingSenderId: "886191631463",
+      appId: "1:886191631463:web:be2d21b93616ea275438c2",
+      measurementId: "G-Z3T3PCJ9V8"
+      });
+  
