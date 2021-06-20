137699f654bfcdc2a4dd001b767ace06c08587ae
pweb50oudcc <85596321+pweb50oudcc@users.noreply.github.com>
2021-06-20 12:34:15 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyBO-WHaei69p0B4ivg_tBG-3ulKqONw0ac",
-    authDomain: "proyectofinal50.firebaseapp.com",
-    projectId: "proyectofinal50",
-    storageBucket: "proyectofinal50.appspot.com",
-    messagingSenderId: "1063502962250",
-    appId: "1:1063502962250:web:d35efac8a2a09d089490b3",
-    measurementId: "G-P3X7E6Y34F"
+  apiKey: "AIzaSyA6y5DufaKY0S4m2KEHgRpha54PmFsuhsA",
+    authDomain: "fbwebauth-66caf.firebaseapp.com",
+    projectId: "fbwebauth-66caf",
+    storageBucket: "fbwebauth-66caf.appspot.com",
+    messagingSenderId: "886191631463",
+    appId: "1:886191631463:web:be2d21b93616ea275438c2",
+    measurementId: "G-Z3T3PCJ9V8"
     });
